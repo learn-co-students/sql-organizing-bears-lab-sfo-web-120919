4e2b861870e4f95499ad0bdd2a8d7762dd053712
brookebachman00 <yourpetlover4411@yahoo.com>
2019-12-18 16:42:23 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 27, "female", "white", "aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Rowdy", 3 , "female", "white", "agressive", 1);
+ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 16 , "male", "black", "sweet", 1);
+ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 20 , "female", "purple", "sensitive", 1);
+ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 54, "male", "yellow", "emotional", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 22 , "male", "light yellow", "angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 9 , "male", "grey", "wild", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Brooke", 25, "female", "blue", "kind", 0);
+
+
